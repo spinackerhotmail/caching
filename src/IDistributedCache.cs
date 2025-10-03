8bdbabd90461806fb95b6a28fc310f0a6bba879d
Sergey Poleshchuk <sergey.poleschuk@leomax.ru>
2025-10-03 22:15:07 +0300

    ача

---
+++ src/IDistributedCache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public interface IDistributedCache
+{
+    byte[]? Get(string key);
+    Task<byte[]?> GetAsync(string key, CancellationToken token = default);
+
+    void Set(string key, byte[] value, DistributedCacheEntryOptions options);
+    Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token = default);
+
+    void Refresh(string key);
+    Task RefreshAsync(string key, CancellationToken token = default);
+
+    void Remove(string key);
+    Task RemoveAsync(string key, CancellationToken token = default);
+}
